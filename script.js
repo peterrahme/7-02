9e80d2fb9e4c8ea3959ecf024a2de5505a16ac6d
peterrahme <peter.rahme@ocsbstudent.ca>
2021-02-18 18:25:37 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Connects "- or +" button to "comparison" function
+document.getElementById('button').addEventListener('click', comparison)
+let userInput = 0
+
+function comparison () {
+  //get User input and convert to an integer
+  userInput = document.getElementById('input').value 
+  userInput = parseInt(userInput)
+
+  // check if userInput is less then 0
+  if (userInput < 0) {
+    document.getElementById('answer').innerHTML = 'negative'
+  } else {
+    document.getElementById('answer').innerHTML = 'positive'
+  }
+}
